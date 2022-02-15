218b69b6285ae1cd0d12ca13f64461006dc9735e
Q U A N G <nts@uwu.trinity.local>
2022-02-14 23:09:59 -0600

    first check

--- src/main/scala/cs2/units/Volume.scala
+++ src/main/scala/cs2/units/Volume.scala
@@ -4,43 +4,132 @@ class Volume(private var lit:Double = 0.0) {
   //Field is in the argument list above - the volume stored in LITERS
 
   //Basic math operators to add, subtract, and scale volumes
-  def + (other:Volume):Volume = { ??? }
-  def += (other:Volume):Unit  = { ??? }
+  def + (other:Volume):Volume = {
+  if(this.lit>0&&other.lit>0) new Volume(this.lit + other.lit)
+  else this
+  }
+  
+  def += (other:Volume):Unit  = { 
+    def lit_=(n:Double):Unit= {
+      lit = lit + n
+    }
+    if(other.lit>0&&this.lit>0)lit_=(other.lit)
+  }
 
-  def - (other:Volume):Volume = { ??? }
-  def -= (other:Volume):Unit  = { ??? }
+  def - (other:Volume):Volume = {
+    if(other.lit < this.lit && this.lit>0 && other.lit>0) {
+    new Volume(this.lit - other.lit)
+    } else {
+      this
+    }
+  }
+  def -= (other:Volume):Unit  = {
+    def lit_=(n:Double):Unit= {
+      lit = lit - n
+    }
+    if(this.lit>other.lit&&this.lit>0&&other.lit>0)lit_=(other.lit)
+  }
 
-  def * (scalar:Double):Volume = { ??? }
-  def *= (scalar:Double):Unit  = { ??? }
+  def * (scalar:Double):Volume = {
+    if(scalar >0&&this.lit>0) {new Volume(this.lit * scalar)}
+    else {this}
+  }
+  def *= (scalar:Double):Unit  = {
+    if(scalar>0&&this.lit>0)
+    {def lit_=(n:Double):Unit= {
+      lit = lit * n
+    }
+    lit_=(scalar)
+  }
+  }
 
-  def / (scalar:Double):Volume = { ??? }
-  def /= (scalar:Double):Unit  = { ??? }
+  def / (scalar:Double):Volume = {
+    if(scalar >0 && this.lit>0) {new Volume(this.lit / scalar)}
+    else {this}
+  }
+  def /= (scalar:Double):Unit  = {
+     if(scalar>0 && this.lit>0)
+    {def lit_=(n:Double):Unit= {
+      lit = lit / n
+    }
+    lit_=(scalar)
+    }
+  }
 
   //Getter functions that return in a variety of units
-  def liters():Double = { ??? }
-  def milliliters():Double = { ??? }
-  def gallons():Double = { ??? }
-  def quarts():Double = { ??? }
-  def pints():Double = { ??? }
-  def cups():Double = { ??? }
-  def teaspoons():Double = { ??? }
-  def tablespoons():Double = { ??? }
+  def liters():Double = {
+    lit
+  }
+  def milliliters():Double = {
+    lit * 1000
+  }
+  def gallons():Double = {
+    lit * 0.264172
+  }
+  def quarts():Double = {
+    lit*1.05669
+  }
+  def pints():Double = {
+    lit * 2.11338
+  }
+  def cups():Double = {
+    lit * 4.22675
+  }
+  def teaspoons():Double = {
+    lit * 202.884
+  }
+  def tablespoons():Double = {
+    lit * 67.628
+  }
 }
+
 
 object Volume {
   //"Constructor" apply methods operating in liters
-  def apply():Volume = { ??? }
-  def apply(amt:Double):Volume = { ??? }
+  def apply():Volume = {
+    new Volume(0)
+  }
+  def apply(amt:Double):Volume = {
+    new Volume(amt)
+  }
 
   //Alternative "static" methods to create volumes in other units
-  def liters(amt:Double):Volume = { ??? } //identical to an apply method
-  def milliliters(amt:Double):Volume = { ??? }
-  def gallons(amt:Double):Volume = { ??? }
-  def quarts(amt:Double):Volume = { ??? }
-  def pints(amt:Double):Volume = { ??? }
-  def cups(amt:Double):Volume = { ??? }
-  def teaspoons(amt:Double):Volume = { ??? }
-  def tablespoons(amt:Double):Volume = { ??? }
+  def liters(amt:Double):Volume = {
+    val litVol = Volume(amt)
+    litVol
+  } //identical to an apply method
+  def milliliters(amt:Double):Volume = {
+    val milVol = Volume(amt/1000)
+    milVol
+  }
+  def gallons(amt:Double):Volume = {
+    val galVol = Volume(amt/0.264172)
+    galVol
+  }
+  def quarts(amt:Double):Volume = {
+    val quaVol = Volume(amt/1.05669)
+    quaVol
+  }
+  def pints(amt:Double):Volume = {
+    val pinVol = Volume(amt/2.11338)
+    pinVol
+  }
+  def cups(amt:Double):Volume = {
+    val cupVol = Volume(amt/4.22675)
+    cupVol
+  }
+  def teaspoons(amt:Double):Volume = {
+    val teaVol = Volume(amt/202.884)
+    teaVol
+  }
+  def tablespoons(amt:Double):Volume = {
+    val tabVol = Volume(amt/67.628)
+    tabVol
+  }
+  def main(args:Array[String]) {
+    println(tablespoons(12).liters())
+    println(gallons(35).liters())
 }
-//testing
+}
+
 
