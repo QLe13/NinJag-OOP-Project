441c4feb3416a016006899dd0752192708244abb
Q U A N G <nts@uwu.trinity.local>
2022-02-23 09:58:43 -0600

    commit

--- src/main/scala/cs2/util/Vec2.scala
+++ src/main/scala/cs2/util/Vec2.scala
@@ -4,107 +4,66 @@ class Vec2 (var x:Double, var y:Double) {
   override def toString():String = "("+x+","+y+")"
   //Methods for addition and subtraction of vectors
   def +  (other:Vec2):Vec2 = {
-    if(this.x !=0 && this.y !=0 && other.x != 0 && other.y !=0){
       new Vec2(this.x+other.x,this.y + other.y)
-    } else this
   }
   def += (other:Vec2):Unit = {
-    if(this.x !=0 && this.y !=0 && other.x != 0 && other.y !=0){
       this.x = this.x + other.x
       this.y = this.y + other.y
     }
-  }
-  
   def -  (other:Vec2):Vec2 = {
-    if(this.x !=0 && this.y !=0 && other.x != 0 && other.y !=0){
       new Vec2(this.x-other.x,this.y-other.y)
-    } else this
-  }
+    }
   def -= (other:Vec2):Unit = {
-    if(this.x !=0 && this.y !=0 && other.x != 0 && other.y !=0){
       this.x = this.x - other.x
       this.y = this.y - other.y
     }
-  }
-
   //Methods for multiplication and division of vectors by a scalar (non-vector)
   def *  (scalar:Double):Vec2 = {
-    if(this.x != 0 && this.y != 0) {
       new Vec2(this.x * scalar, this.y * scalar)
-    } else this
-  }
+    } 
   def *= (scalar:Double):Unit = {
-    if(this.x != 0 && this.y != 0) {
       this.x = this.x * scalar
       this.y = this.y * scalar
     }
-  }
 
   def /  (scalar:Double):Vec2 = {
-    if(this.x != 0 && this.y != 0 && scalar !=0){
       new Vec2(this.x / scalar, this.y / scalar)
-    } else this
-  }
-  def /= (scalar:Double):Unit = {
-    if(this.x != 0 && this.y != 0 && scalar !=0){
-      this.x = this.x / scalar
-      this.y = this.y /scalar
-    } else if (scalar != 0){
-      this.x = this.x / scalar
-      this.y = this.y /scalar
     }
-  }
+  def /= (scalar:Double):Unit = {
+      this.x = this.x / scalar
+      this.y = this.y / scalar 
+    }
 
   //Methods to determine the length of a vector (magnitude and length should return the same value)
   def magnitude():Double = {
-    if(this.x != 0 && this.y != 0){
      math.sqrt(math.pow(this.x,2)+math.pow(this.y,2))
-    } else {
-       0
     }
-  }
   def length():Double = {
-    if(this.x != 0 && this.y != 0){
      math.sqrt(math.pow(this.x,2)+math.pow(this.y,2))
-    } else {
-       0
     }
-  }
   
   //Methods to calculate the dot product (same returns)
   def dot(other:Vec2):Double = {
-    if(this.x != 0 && this.y != 0 && other.x != 0 && other.y != 0){
       this.x * other.x + this.y * other.y
-    } else {
-      0
     }
-  }
   def **(other:Vec2):Double = {
-    if(this.x != 0 && this.y != 0 && other.x != 0 && other.y != 0){
       this.x * other.x + this.y * other.y
-    } else {
-      0
     }
-  }
   
   //Methods to determine the angle between 2 vectors (same returns)
   def angleBetween(other:Vec2):Double = {
-    if(this.length!=0 && other.length != 0) math.acos(this.dot(other)/(this.length()*other.length()))
-    else 0
+     math.acos(this.dot(other)/(this.length()*other.length()))
   }
   def <>(other:Vec2):Double = {
-    if(this.length!=0 && other.length != 0) math.acos(this.dot(other)/(this.length()*other.length()))
-    else 0
+      math.acos(this.dot(other)/(this.length()*other.length()))
   }
 
   //Methods to return a new vector that is in the same direction, but length 1 (same returns)
   def normalize():Vec2 = {
-    if(this.x !=0 && this.y !=0) new Vec2(this.x/this.length,this.y/this.length)
-    else new Vec2(0,0)
+      new Vec2(this.x/this.length,this.y/this.length)
   }
   def unary_~ : Vec2 = {
-    if(this.x !=0 && this.y !=0) new Vec2(this.x/this.length,this.y/this.length)
-    else new Vec2(0,0)
+      new Vec2(this.x/this.length,this.y/this.length)
   }
 
   //A clone operator can be useful when making "deep" copies of objects
@@ -132,6 +91,7 @@ object Vec2 {
      if(startPoint.x < 0 && startPoint.y > 0) println(startPoint.y + " N, "+ -startPoint.x+" W")
      if(startPoint.x > 0 && startPoint.y < 0) println(-startPoint.y + " S, "+ startPoint.x+" E")
      if(startPoint.x < 0 && startPoint.y < 0) println(-startPoint.y + " S, "+ -startPoint.x+" W")
+     
   }
 }
 //just a comment line to check the github
